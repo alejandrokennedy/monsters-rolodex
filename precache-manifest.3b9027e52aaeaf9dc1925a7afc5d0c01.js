a76987c1528ccf4dacc7989304256e50cd4db747
alejandrokennedy <alejandrokennedy@gmail.com>
2020-07-08 14:55:23 -0700

    Updates

---
+++ precache-manifest.3b9027e52aaeaf9dc1925a7afc5d0c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e46c7e34e5fc7f5c0f8479070b3bd93",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "510582bfe0747450a36e",
+    "url": "/monsters-rolodex/static/css/main.42efb1e2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510582bfe0747450a36e",
+    "url": "/monsters-rolodex/static/js/main.6017c2ca.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
